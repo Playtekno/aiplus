bdfb1475fe6b3157d59d8b91b1e0d8b5d97adc60
Playtekno <143980987+Playtekno@users.noreply.github.com>
2023-12-21 12:46:24 +0700

    Update optimg.js

--- optimg.js
+++ optimg.js
@@ -2,7 +2,7 @@
 const cards = document.querySelectorAll('.card');
 
 // URL yang akan ditambahkan sebelum URL gambar
-const urlPrefix = 'https://res.cloudinary.com/practicaldev/image/fetch/s--BlMrNwoZ--/c_limit,f_webp,fl_progressive,q_80,h_200,w_348/';
+const urlPrefix = 'https://res.cloudinary.com/practicaldev/image/fetch/s--BlMrNwoZ--/c_limit,f_avif,fl_progressive,q_80,h_200,w_348/';
 
 // Loop melalui setiap elemen dengan kelas "card"
 cards.forEach((card) => {
@@ -14,14 +14,8 @@ cards.forEach((card) => {
     // Mendapatkan URL gambar asli
     const imageUrl = image.getAttribute('src');
 
-    // Mengecek apakah URL gambar berformat .avif
-    const isAvif = imageUrl.endsWith('?format=.webp');
-
-    // Mengubah format gambar menjadi .avif jika belum .avif
-    const formattedUrl = isAvif ? imageUrl : imageUrl.replace(/\.[^/.]+$/, '?format=.webp');
-
     // Menggabungkan URL prefix dengan URL gambar
-    const modifiedUrl = urlPrefix + formattedUrl;
+    const modifiedUrl = urlPrefix + imageUrl;
 
     // Mengatur URL gambar yang telah dimodifikasi
     image.setAttribute('src', modifiedUrl);
