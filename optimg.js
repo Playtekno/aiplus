550637f02b800cc96266de49ae42d927448fcd47
Playtekno <143980987+Playtekno@users.noreply.github.com>
2023-12-21 16:38:40 +0700

    Update optimg.js

--- optimg.js
+++ optimg.js
@@ -1,6 +1,9 @@
 // Mendapatkan semua elemen dengan kelas "card"
 const cards = document.querySelectorAll('.card');
 
+// URL yang akan ditambahkan sebelum URL gambar
+const urlPrefix = 'https://res.cloudinary.com/practicaldev/image/fetch/s--BlMrNwoZ--/c_limit,f_avif,fl_progressive,q_80,h_200,w_348/';
+
 // Mendapatkan tanggal, bulan, dan tahun saat ini
 const currentDate = new Date();
 const day = currentDate.getDate().toString().padStart(2, '0');
@@ -20,8 +23,8 @@ cards.forEach((card) => {
     // Mendapatkan URL gambar asli
     const imageUrl = image.getAttribute('src');
 
-    // Menggabungkan URL gambar asli dengan parameter "?v=tanggalbulatahun"
-    const modifiedUrl = imageUrl + '?v=' + formattedDate;
+    // Menggabungkan URL prefix dengan URL gambar asli
+    const modifiedUrl = urlPrefix + imageUrl + '?v=' + formattedDate;
 
     // Mengatur URL gambar yang telah dimodifikasi
     image.setAttribute('src', modifiedUrl);
