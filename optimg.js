1ab92f84e3ab6d72d68305377e3b17c6a6f72e48
Playtekno <143980987+Playtekno@users.noreply.github.com>
2023-12-21 13:25:29 +0700

    Update optimg.js

--- optimg.js
+++ optimg.js
@@ -1,6 +1,9 @@
 // Mendapatkan semua elemen dengan kelas "card"
 const cards = document.querySelectorAll('.card');
 
+// URL yang akan ditambahkan sebelum URL gambar asli
+const urlPrefix = 'https://res.cloudinary.com/practicaldev/image/fetch/s--BlMrNwoZ--/c_limit,f_avif,fl_progressive,q_80,h_200,w_348/';
+
 // Loop melalui setiap elemen dengan kelas "card"
 cards.forEach((card) => {
   // Mendapatkan semua elemen gambar di dalam elemen card
@@ -14,8 +17,8 @@ cards.forEach((card) => {
     // Mendapatkan format gambar
     const imageFormat = imageUrl.substring(imageUrl.lastIndexOf('.') + 1);
 
-    // Menggabungkan URL gambar asli dengan prefix "?format=.avif"
-    const modifiedUrl = imageUrl + '?format=.avif';
+    // Menggabungkan URL prefix dengan URL gambar asli
+    const modifiedUrl = urlPrefix + imageUrl + '?format=.avif';
 
     // Mengatur URL gambar yang telah dimodifikasi
     image.setAttribute('src', modifiedUrl);
