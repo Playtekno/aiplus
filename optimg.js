e6402d73ebe3b8649567cfa1d68f98d08bd01748
Playtekno <143980987+Playtekno@users.noreply.github.com>
2023-12-21 12:39:09 +0700

    Update optimg.js

--- optimg.js
+++ optimg.js
@@ -1,78 +1,29 @@
-// Optimize website
+// Mendapatkan semua elemen dengan kelas "card"
+const cards = document.querySelectorAll('.card');
 
-// Auto add image size width that fits with the div class
-const divClass = document.querySelector('.div-class');
-const image = document.createElement('img');
-image.src = 'image.jpg';
-image.onload = function() {
-  const width = image.width;
-  divClass.style.width = `${width}px`;
-};
+// URL yang akan ditambahkan sebelum URL gambar
+const urlPrefix = 'https://res.cloudinary.com/practicaldev/image/fetch/s--BlMrNwoZ--/c_limit,f_avif,fl_progressive,q_80,h_200,w_348/';
 
-// Minimize payload size and number of requests
-// Combine and minify CSS and JavaScript files
-// Use CSS sprites for multiple small images
+// Loop melalui setiap elemen dengan kelas "card"
+cards.forEach((card) => {
+  // Mendapatkan semua elemen gambar di dalam elemen card
+  const images = card.querySelectorAll('img');
 
-// Set appropriate data-src attributes for the lazy images and add CSS class 'lazy'
-const lazyImages = document.querySelectorAll('img.lazy');
-lazyImages.forEach((lazyImage) => {
-  const imageSrc = lazyImage.getAttribute('src');
-  const imageExtension = imageSrc.substr(imageSrc.lastIndexOf('.') + 1).toLowerCase();
-  const supportedFormats = ['png', 'jpg', 'jpeg'];
+  // Loop melalui setiap elemen gambar
+  images.forEach((image) => {
+    // Mendapatkan URL gambar asli
+    const imageUrl = image.getAttribute('src');
 
-  if (supportedFormats.includes(imageExtension)) {
-    const webpImageSrc = imageSrc.replace(/\.(png|jpg|jpeg)$/i, '.webp');
-    lazyImage.dataset.src = webpImageSrc;
-    lazyImage.classList.add('lazy');
-  } else {
-    lazyImage.dataset.src = imageSrc;
-    lazyImage.classList.add('lazy');
-  }
-});
+    // Mengecek apakah URL gambar berformat .avif
+    const isAvif = imageUrl.endsWith('.avif');
 
-// Lazy load non-critical assets
-const lazyLoadImages = () => {
-  const lazyImages = document.querySelectorAll('.lazy');
-  lazyImages.forEach((lazyImage) => {
-    if (lazyImage.offsetTop < window.innerHeight + window.pageYOffset) {
-      const imageSrc = lazyImage.dataset.src;
-      const imageExtension = imageSrc.substr(imageSrc.lastIndexOf('.') + 1).toLowerCase();
-      const supportedFormats = ['png', 'jpg', 'jpeg'];
+    // Mengubah format gambar menjadi .avif jika belum .avif
+    const formattedUrl = isAvif ? imageUrl : imageUrl.replace(/\.[^/.]+$/, '.avif');
 
-      if (supportedFormats.includes(imageExtension)) {
-        const webpImageSrc = imageSrc.replace(/\.(png|jpg|jpeg)$/i, '.webp');
-        lazyImage.src = webpImageSrc;
-        lazyImage.classList.add('webp');
-      } else {
-        lazyImage.src = imageSrc;
-      }
+    // Menggabungkan URL prefix dengan URL gambar
+    const modifiedUrl = urlPrefix + formattedUrl;
 
-      lazyImage.classList.remove('lazy');
-    }
+    // Mengatur URL gambar yang telah dimodifikasi
+    image.setAttribute('src', modifiedUrl);
   });
-};
-
-window.addEventListener('scroll', lazyLoadImages);
-window.addEventListener('resize', lazyLoadImages);
-window.addEventListener('orientationchange', lazyLoadImages);
-
-// Handle changes efficiently
-// Use event delegation for dynamic elements
-
-// Use modern browser features
-// Utilize Intersection Observer API for lazy loading
-
-// Example of handling changes efficiently using event delegation
-document.addEventListener('click', function(event) {
-  if (event.target.matches('.dynamic-element')) {
-    // Handle dynamic element click
-  }
-});
-
-// Auto add alt and title from page title to the image
-const pageTitle = document.title;
-const lazyImages = document.querySelectorAll('.lazy');
-lazyImages.forEach((lazyImage) => {
-  lazyImage.alt = pageTitle;
-  lazyImage.title = pageTitle;
 });
